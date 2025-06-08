7f22580a5f7856388c9812fcaa552d996f2a01ff
akram-abid <a.abid44778@gmail.com>
2025-06-08 17:52:01 +0100

    require the db

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require("node:path");
 const session = require("./config/session")
 const memberController = require("./controllers/membersController")
 const passport = require("./config/passport")
+const db = require("./db/pool");
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -21,12 +23,6 @@ app.use(session);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }))
-app.use(async (req, res) => {
-    const result = await db.query(
-      "SELECT 1 FROM pg_database WHERE datname = 'onlymembers'"
-    );
-    console.log("okay this is it the database ", result)
-})
 app.use(memberController.checkSession);   
 
 app.use("/", routes);
